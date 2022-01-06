605f99f72be27a488c8cdd912c641b6be818ce40
JaqKent <jaq.kent@gmail.com>
2022-01-06 13:23:57 -0300

    feedback-Item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./Components/Header";
 import FeedbackList from "./Components/FeedbackList";
 import FeedbackStats from "./Components/FeedbackStats";
 import FeedbackData from "./Data/FeedbackData";
+import FeedbackForm from "./Components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -15,6 +21,8 @@ function App() {
     <>
       <Header />
       <div className="container">
+        import Feedbackform from "./Components/FeedbackForm";
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
--- src/Components/Shared/card.jsx
+++ src/Components/Shared/card.jsx
@@ -2,6 +2,6 @@ function Card({ children, reverse }) {
   return <div className={`card ${reverse && "reverse"}`}>{children}</div>;
 }
 Card.defaultProps = {
-  reverse: true,
+  reverse: false,
 };
 export default Card;
