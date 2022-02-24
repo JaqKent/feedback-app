31262188667abcac46ed3e2c75dcb28a614a7d58
JaqKent <jaq.kent@gmail.com>
2022-02-24 12:26:26 -0300

    finish

--- src/Components/context/FeedbackContext.js
+++ src/Components/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { v4 as uuidv4 } from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext();
@@ -16,24 +15,42 @@ export const FeedbackProvider = ({ children }) => {
   useEffect(() => { fetchFeedback() }, [])
 
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
     const data = await response.json()
     setFeedback(data);
     setIsLoading(false);
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+    const data = await response.json()
+    setFeedback([data, ...feedback]);
   };
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
+
+      await fetch(`/feedback/${id}`, { method: "DELETE" })
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+
+    })
+    const data = await response.json()
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item))
   }
 
   const editFeedback = (item) => {
